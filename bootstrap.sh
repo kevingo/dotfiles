5a71fc64a87a5d22bcba9c2727f599ed0bd2837a
kevingo <kevingo75@gmail.com>
2016-08-26 13:49:39 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,14 +5,14 @@ cd "$(dirname "${BASH_SOURCE}")";
 git pull origin master;
 
 function doIt() {
-	rsync --exclude ".git/" \
+	rsync   --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude ".osx" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
-	source ~/.bash_profile;
+	source ~/.bashrc;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
