9355103e1195422878f7956d42910ba790e4b097
kevingo <kevingo75@gmail.com>
2016-08-26 18:26:28 +0800

    Update nvm.sh

--- nvm.sh
+++ nvm.sh
@@ -5,8 +5,5 @@ if test ! $(which nvm); then
     curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.6/install.sh | bash
 fi
 
-# reload nvm into this environment
-source $(brew --prefix nvm)/nvm.sh
-
 nvm install stable
 nvm alias default stable
